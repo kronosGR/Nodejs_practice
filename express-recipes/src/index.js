e2ecc9203a2a02a62c8f9266fd0aa7af83b5e08e
KronosGR <geo.elgeo@gmail.com>
2022-09-16 07:49:48 +0200

    added simple routing and installed nodemon

--- express-recipes/src/index.js
+++ express-recipes/src/index.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 8000
 
-app.listen(port, ()=>{
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
+app.get("/:name", (req,res)=> {
+  res.send(`Welcome ${req.params.name}`);
+})
+
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 })
