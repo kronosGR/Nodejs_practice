acf852d8f74ac45311ce083bf8bed09d1d916eb3
KronosGR <geo.elgeo@gmail.com>
2022-09-16 08:12:34 +0200

    added middleware to log requests

--- express-recipes/src/index.js
+++ express-recipes/src/index.js
@@ -1,21 +1,28 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const publicDirectoryPath = path.join(__dirname, './public');
-app.use(express.static(publicDirectoryPath))
+const publicDirectoryPath = path.join(__dirname, "./public");
+app.use(express.static(publicDirectoryPath));
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
+
+app.use((req, res, next) => {
+  const { method, path } = req;
+  console.log(
+    `New request to: ${method} ${path} at ${new Date().toDateString()}`
+  );
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello world")
-})
+  res.send("Hello world");
+});
 
-app.get("/:name", (req,res)=> {
+app.get("/:name", (req, res) => {
   res.send(`Welcome ${req.params.name}`);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`)
-})
+  console.log(`Server is up on port ${port}`);
+});
