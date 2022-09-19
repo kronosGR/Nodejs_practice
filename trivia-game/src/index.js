59c21c0fa8a497ecdda8f92829d89182ab4dfba6
KronosGR <geo.elgeo@gmail.com>
2022-09-19 08:08:50 +0200

    added socketio and FE

--- trivia-game/src/index.js
+++ trivia-game/src/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
+io.on("connection", () => {
+  console.log("A new player just connected.");
+});
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
