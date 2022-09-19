a335edefebdadf341f2f63af2df5d3079c9a5b70
KronosGR <geo.elgeo@gmail.com>
2022-09-19 08:18:28 +0200

    added logic

--- trivia-game/src/index.js
+++ trivia-game/src/index.js
@@ -3,6 +3,15 @@ const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
 
+const formatMessage = require("./utils/formatMessage");
+
+const {
+  addPlayer,
+  getAllPlayers,
+  getPlayer,
+  removePlayer,
+} = require("./utils/players");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -10,8 +19,17 @@ const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("A new player just connected.");
+
+  socket.on("join", ({playerName, room}, callback) => {
+    const {error, newPlayer} = addPlayer({id: socket.id, playerName, room})
+
+    if (error) return callback(error.message)
+    callback()
+    socket.join(newPlayer.room)
+    socket.emit("message", formatMessage("Admin", "Welcome"))
+  })
 });
 
 const port = process.env.PORT || 8000;
